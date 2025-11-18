33dbc65419f57d3f927386457d86142949d6b0bf
Adam <petrzela.adam@protonmail.com>
2025-11-18 18:40:18 +0100

    feat: setup commands

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,27 @@
-import { readConfig, setUser } from './config';
+import {
+  CommandsRegistry,
+  handlerLogin,
+  registerCommand,
+  runCommand,
+} from './command';
 
 function main() {
-  setUser('Adam');
-  const config = readConfig();
-  console.log(config);
+  const commands: CommandsRegistry = {};
+  registerCommand(commands, 'login', handlerLogin);
+  const rawArgs = process.argv.slice(2);
+  if (rawArgs.length === 0) {
+    console.error("Not enough arguments were provided");
+    process.exit(1);
+  }
+
+  const [cmdName, ...args] = rawArgs;
+  
+  try {
+    runCommand(commands, cmdName, ...args);
+  } catch (error) {
+    console.error(error instanceof Error ? error.message : String(error));
+    process.exit(1);
+  }
 }
 
 main();
